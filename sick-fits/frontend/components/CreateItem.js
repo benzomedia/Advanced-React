f8bbc4413ea781b79076c135fe2261a4122b7761
Oren Reuveni <oren@benzo.io>
2019-03-23 18:29:04 +0200

    Cloudinary - upload iamge

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -43,6 +43,29 @@ class CreateItem extends Component {
     });
   };
 
+  uploadFile = async e => {
+    console.log('uploading file...');
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/benzo/image/upload',
+      {
+        method: 'post',
+        body: data
+      }
+    );
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     const { title, description, image, largeImage, price } = this.state;
     return (
@@ -60,6 +83,18 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                File
+                <input
+                  type="file"
+                  id="file"
+                  placeholder="Upload an image"
+                  name="file"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {image && <img width="200" src={image} alt="Upload Preview" />}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
